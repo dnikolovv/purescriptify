3bbade6f6f4e7a7f514e2b3c1ae7fb534bb84761
Dobromir Nikolov <dnikolovv@hotmail.com>
2022-08-02 20:11:31 +0300

    Remove unneeded dependencies, add firebase.

--- dev/index.js
+++ dev/index.js
@@ -1,4 +1,18 @@
-import 'react-toastify/dist/ReactToastify.css';
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyClKMpxtbTKkrJDo7RmUV8Ucm6KLfDzoCo",
+  authDomain: "purescriptify.firebaseapp.com",
+  projectId: "purescriptify",
+  storageBucket: "purescriptify.appspot.com",
+  messagingSenderId: "475139771273",
+  appId: "1:475139771273:web:423e214baf655eede76f58",
+  measurementId: "G-NVC093SV8B"
+};
+
+const app = initializeApp(firebaseConfig);
+getAnalytics(app);
 
 require('@fontsource/inter');
 require("../output/Main/index.js").main();
